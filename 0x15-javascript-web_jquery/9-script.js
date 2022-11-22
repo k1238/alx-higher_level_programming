b3196c7bf99c9c2f2a04ef3848206a33ac24d17d
k1238 <kalebadino@gmail.com>
2022-11-22 22:20:49 +0200

    task 9

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  const salutUri = 'https://fourtonfish.com/hellosalut/?lang=fr';
+  const $helloElement = $('div#hello');
+
+  function getSalut () {
+    return $.get({
+      url: salutUri,
+      dataType: 'json'
+    });
+  }
+
+  getSalut().then((res) => {
+    $helloElement.text(res.hello);
+  });
+});
+
+
